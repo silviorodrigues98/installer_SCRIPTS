5c1e641dc25e2f8edd7ddb9633b0f2b9485018b9
Silvio Rodrigues Correa <silviorodrigues98@hotmail.com>
2022-11-02 14:33:58 -0300

    Update installDockerCLIonly.sh

--- installDockerCLIonly.sh
+++ installDockerCLIonly.sh
@@ -25,6 +25,8 @@ echo \
 sudo apt-get update
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo usermod -aG docker $USER
+
 echo 
 echo
 echo ----------------------------------------------------------------------------------------------------------------------------------------------------------
